65d33e216d7b25025e4f4dbc87ad279414b30c20
DENDENICH <daniil.gol26@yandex.ru>
2025-01-22 16:36:25 +0300

    Create __init__ sql query for create table user

--- data_base/db.py
+++ data_base/db.py
@@ -1,12 +1,20 @@
 import sqlite3
 
 
-class DB:
+class User:
     """Class for works with db sqlite3"""
     def __init__(self, path: str = "prog_db.db"):
         
         self.con = sqlite3.connect(path)
         self.cur = self.con.cursor()
+
+        self.cur.execute("""CREATE TABLE user IF NOT EXISTS (
+                        id INTEGER PRIMARY KEY;
+                        name TEXT;
+                        contact TEXT;
+                        
+                         )
+                         """)
 
     def register_user(
             name: str,
