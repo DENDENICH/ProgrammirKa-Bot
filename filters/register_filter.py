0cc0c3b8b13f065dd12beb37255dc007726862bd
DENDENICH <daniil.gol26@yandex.ru>
2025-01-24 00:15:05 +0300

    add filters for phone number

---
+++ filters/register_filter.py
@@ -0,0 +1,19 @@
+from aiogram.types import Message
+
+import re
+
+def is_correct_phone_number(message: Message) -> bool:
+    """Проверяет, является ли строка корректным российским номером телефона.
+
+        Правила:
+        - startwith +7 or 8.
+        - ten numbers.
+        - space, defice is correct.
+        """
+    pattern = re.compile(r'^(\+7|8)\s*\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$')
+    return bool(pattern.match(message.text.strip()))
+
+
+def is_not_register_student(message: Message) -> bool:
+    # TODO: проверка присутствия ученика в БД по id его ТГ
+    pass
