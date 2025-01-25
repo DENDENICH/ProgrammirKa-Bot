1b29f9536eaeac681677a517c8cecfacc571a262
DENDENICH <daniil.gol26@yandex.ru>
2025-01-25 10:50:47 +0300

    modified class fo sqlite: add asyncio

--- data_base/db.py
+++ data_base/db.py
@@ -1,29 +1,39 @@
-import sqlite3
+import aiosqlite
 
 
 class User:
     """Class for works with db sqlite3"""
-    def __init__(self, path: str = "prog_db.db"):
-        
-        self.con = sqlite3.connect(path)
-        self.cur = self.con.cursor()
 
-        self.cur.execute("""CREATE TABLE user IF NOT EXISTS (
-                         id INTEGER PRIMARY KEY;
-                         name TEXT;
-                         contact TEXT;
-                         homework TEXT;
-                         progress_stage TEXT;
-                         learning_programm INTEGER
-                         )
-                         """)
+    def __init__(self):
+        self.connect = None
 
-    def register_user(self) -> None:
+
+    async def initialize_bd(self, path: str = "db.db"):
+        """Initialize bd, create tables
+        :param path: path to file database
+        """
+        self.connect = await aiosqlite.connect(path)
+
+        # initialize table user
+        async with self.connect as connect:
+            await connect.execute("""CREATE TABLE user IF NOT EXISTS (
+                             id INTEGER PRIMARY KEY NOT NULL;
+                             tg_id INTEGER UNIQUE NOT NULL
+                             name TEXT NOT NULL;
+                             contact TEXT NOT NULL;
+                             homework TEXT;
+                             homework_completed BOOLEAN NOT NULL;
+                             count_help_homework INTEGER NOT NULL;
+                             progress_stage TEXT NOT NULL;
+                             )
+                             """)
+
+    async def register_user(self) -> None:
         """Method for register user"""
         pass
 
 
-    def set_homework(self) -> None:
+    async def set_homework(self) -> None:
         """Method for set new homework"""
         pass
 
