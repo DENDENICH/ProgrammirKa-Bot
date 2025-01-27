b26b0f9a0f29a8b30bd73170e07e84b3e25bba28
DENDENICH <daniil.gol26@yandex.ru>
2025-01-27 11:51:37 +0300

    to transfer bot and dp from main in config

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from aiogram import Dispatcher, Bot
-from aiogram.fsm.storage.redis import RedisStorage
 
-from redis.asyncio import Redis
 
-from config import tg_config
+from config import tg_config, bot, dp
 
 
 async def main() -> None:
+    pass
 
-    # set redis storage for FSM
-    redis = Redis(host="localhost")
-    storage = RedisStorage(redis=redis)
-
-    # initialize bot and he config
-    bot = Bot(token=tg_config.token)
-    dp = Dispatcher(storage=storage)
 
     # TODO: инициализация и регистрация роутеров
